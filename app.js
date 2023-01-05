4518c11f03353bde355ce176eba394ad4a6202aa
ezeisrael61c <ezeisrael61c@gmail.com>
2023-01-04 22:08:11 -0300

    c

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+
 app.use(express.static("public"))
 
-//app.get("/", (req, res) => {
-//    res.send("hola")
-//})
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
-app.listen(3000, () => 
-console.log("servidor corriendo en puerto 3000"));
+app.listen(port,() => console.log(` server on port ${port}\n http://localhost:${port}`)
+);
